eaeb914063d2a04011594e2e1b75c19828f70fb9
张峰 <zhangfeng@hujiang.com>
2017-12-04 22:55:30 +0800

    03-02-webpack-watch

---
+++ 03-development/02-webpack-watch/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin('./dist')
+  ]
+};
+
+module.exports = config;
