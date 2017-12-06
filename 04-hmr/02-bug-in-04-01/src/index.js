e6526f7a01214bd956251620ca4b099fb35a61f8
张峰 <zhangfeng@hujiang.com>
2017-12-06 15:01:25 +0800

    update: 04-02

--- 04-hmr/02-bug-in-04-01/src/index.js
+++ 04-hmr/02-bug-in-04-01/src/index.js
@@ -1,4 +1,5 @@
 import handler from './handler';
+import './mod';
 
 
 // 创建按钮并注册事件
@@ -11,16 +12,31 @@ function createLogButton(handler) {
   return btn;
 }
 
-const btn = createLogButton(handler);
+let btn = createLogButton(handler);
 document.body.appendChild(btn);
 
 
 // 热加载
-// 尝试修改 handler 模块后，发现热加载前的按钮依然存在
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+// 尝试修改某个模块后（例如 handle 或 mod），发现热加载前的按钮依然存在
 // 更进一步说，每次热更新都会保留旧的按钮
 // 这是因为重复执行了 index.js 模块
-// 虽然可以通过在热加载的回调函数中将旧的 btn 移除，但这肯定不是长久之计
-// 可以使用一些 loader 来解决这类问题，参见 04-04
+// 可以通过在热加载的回调函数中将旧的 btn 移除
+// 可以尝试注释上面的热加载代码，解锁下面的加载代码
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept([
+    './handler',
+    './mod'
+  ], () => {
+    document.body.removeChild(btn);
+
+    btn = createLogButton(handler);
+    document.body.appendChild(btn);
+  });
 }
+
+// 但类似的坑还有很多，手动解决肯定不是长久之计
+// 可以使用一些 loader 来解决这类问题，参见 04-04
---
+++ 04-hmr/02-bug-in-04-01/src/mod.js
@@ -0,0 +1,5 @@
+console.log('hello from mod.');
+
+// console.log('hello from mod, again.');
+
+// console.log('hello from mod, again and again.');
