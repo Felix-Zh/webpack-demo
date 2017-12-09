3f137de07cc9faee1e5e5b06232999aee241e455
张峰 <zhangfeng@hujiang.com>
2017-12-10 01:38:12 +0800

    update: 06-01

--- 06-production/01-two-kind-of-config/webpack.prod.js
+++ 06-production/01-two-kind-of-config/webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
+
+const envMatch = /^build:([a-z]+)$/.exec(process.env.npm_lifecycle_event);
+const env = envMatch ? envMatch[1] : 'dev';
+
 // 注意这里将 uglifyjs-webpack-plugin 作为依赖项进行安装
 // 而并没有使用 webpack.optimize.UglifyJsPlugin
 // 这是因为，webpack 3 自带的 uglifyjs-webpack-plugin 插件版本较低
@@ -12,9 +16,13 @@ const commonConfig = require('./webpack.common');
 
 const config = {
   devtool: 'source-map',
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+  plugins: []
 };
+
+if (env !== 'qa') {
+  config.plugins = config.plugins || [];
+
+  config.plugins.push(new UglifyJsPlugin());
+}
 
 module.exports = merge(commonConfig, config);
