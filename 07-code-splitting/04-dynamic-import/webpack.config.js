d5773ba22336220dd1d6fd464aac72c954252c6c
张峰 <zhangfeng@hujiang.com>
2017-12-13 00:49:45 +0800

    update: 07-04

--- 07-code-splitting/04-dynamic-import/webpack.config.js
+++ 07-code-splitting/04-dynamic-import/webpack.config.js
@@ -5,15 +5,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const config = {
-  entry: './src/index',
+  entry: {
+    app: './src/index',
+    another: './src/another'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
 
     // chunkFilename 用以指定非入口 chunk 的命名规则
     // 异步 chunk 是非入口 chunk，因此将应用这条规则
+    // 默认情况下，异步 chunk 的 [name] 是数字 id
+    // 可以通过 import 时的 webpackChunkName 注释指定这个 [name]
+    // 参见 ./src/index.js 和 ./src/another.js
     chunkFilename: '[name].bundle.js',
 
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
     new CleanWebpackPlugin('./dist'),
