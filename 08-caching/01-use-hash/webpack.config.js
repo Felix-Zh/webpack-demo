a2a9e6e0b5f1ebcb39d155a71a857fd5d1e26a46
张峰 <zhangfeng@hujiang.com>
2017-12-14 11:50:52 +0800

    update: 08-01

--- 08-caching/01-use-hash/webpack.config.js
+++ 08-caching/01-use-hash/webpack.config.js
@@ -13,7 +13,7 @@ const config = {
 
     // 使用 [chunkhash] 占位符表示 hash，使用冒号数字表示 hash 的长度
     // 注意老版本的 webpack 可能会出现含有启动代码的 chunk（即使未修改但）每次构建 hash 都发生变化的情况
-    // 这是因为启动代码（webpack bootstrap）中包含 runtime 和 manifest 代码
+    // 这是因为启动代码（webpack bootstrap）中包含 runtime 和 manifest 这样的样板代码
     // 新版本解决了该问题，但 webpack 仍然建议将这些样板代码抽出，参见 08-02
     filename: '[name].[chunkhash:5].js'
 
